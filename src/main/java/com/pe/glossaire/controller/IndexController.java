08a740e713bb3f2877e61f76ce08a6c3a8348a6f
NEMMICHE Abdel <abdel.nemmiche@pole-emploi.fr>
2018-07-13 18:56:28 +0200

    maj responsive FF17

--- src/main/java/com/pe/glossaire/controller/IndexController.java
+++ src/main/java/com/pe/glossaire/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.pe.glossaire.controller;
 import com.pe.glossaire.model.Acronym;
 import com.pe.glossaire.service.AcronymService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Controller
-
-
-
 public class IndexController {
 
     @Autowired
@@ -33,17 +31,6 @@ public class IndexController {
         return "index";
     }
 
-    @RequestMapping("/indexsav")
-    public String indexsav(Long nombre, Model model, String dateJour){
-        Iterable<Acronym> acronyms = acronymService.retrieve();
-        nombre=acronyms.spliterator().getExactSizeIfKnown();
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-        LocalDate localDate = LocalDate.now();
-        dateJour=dtf.format(localDate);
-        model.addAttribute("dateJour", dateJour);
-        model.addAttribute("nombre", nombre);
-        return "indexsav";
-    }
 
 }
